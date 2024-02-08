722e8111e3fd6f42aeb343f3144c6b3b220efe85
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-08 12:54:04 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/User/CreateUser.tsx
+++ client/src/User/CreateUser.tsx
@@ -6,7 +6,7 @@ const CreateUser = () => {
   return (
     <div className="bg-blue-600 h-screen px-24 py-20">
         <div className="bg-white text-black px-12 py-8 rounded">
-            <Link to="/user"><span>Back</span></Link>
+            <Link to="/user"><span><IonIcon name="arrow-back" />Back</span></Link>
             <p className="font-semibold text-2xl py-4">Add New User</p>
             <hr />
 
