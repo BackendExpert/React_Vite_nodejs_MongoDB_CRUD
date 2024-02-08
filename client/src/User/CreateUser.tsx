c862ce5dd8a548e90186e2d1d5d7e45856bd62fd
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-08 20:14:56 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/User/CreateUser.tsx
+++ client/src/User/CreateUser.tsx
@@ -1,8 +1,21 @@
 import { Link } from 'react-router-dom'
 import IonIcon from "@reacticons/ionicons"
+import React, {useState} from "react"
+import axios from 'axios'
 
 
 const CreateUser = () => {
+  const [name, setName] = useState()
+  const [email, setEmail] = useState()
+  const [age, setAge] = useState()
+  
+  const Submit = (e) => {
+    e.preventDefault();
+    axios.post("http://localhost:3001/createUser", {name, email, age})
+    .then(result => console.log(result))
+    .catch(err => console.log(err))
+  }
+
   return (
     <div className="bg-blue-600 h-screen px-24 py-20">
         <div className="bg-white text-black px-12 py-8 rounded">
@@ -10,18 +23,18 @@ const CreateUser = () => {
             <p className="font-semibold text-2xl py-4">Add New User</p>
             <hr />
 
-            <form>
+            <form onSubmit={Submit}>
               <div className="mb-2">
                 <label htmlFor="">Name</label>
-                <input type="text" name="Enter Name" id="" className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'/>
+                <input onChange={(e) => setName(e.target.value)} type="text" placeholder="Enter Name" className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'/>
               </div>
               <div className="mb-2">
                 <label htmlFor="">Email</label>
-                <input type="email" name="Enter Name" id="" className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'/>
+                <input onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Enter Email" className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'/>
               </div>
               <div className="mb-2">
                 <label htmlFor="">Age</label>
-                <input type="number" name="Enter Name" id="" className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'/>
+                <input onChange={(e) => setAge(e.target.value)} type="number" placeholder="Enter Age" className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'/>
               </div>
               <button className="rounded bg-green-700 duration-500 hover:bg-green-800 text-white py-4 px-8 font-semibold text-md">Add User</button>
             </form>
