f7797a57e62451b47d8701c4a98ba1e22a1cd9a8
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-08 09:06:27 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/User/users.tsx
+++ client/src/User/users.tsx
@@ -4,6 +4,19 @@ const users = () => {
         <div className="bg-white text-black px-12 py-8 rounded">
             <p className="font-semibold text-2xl py-2">Users</p>
             <hr />
+
+            <table className="table-auto">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Age</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr></tr>
+                </tbody>
+            </table>
         </div>
     </div>
   )
