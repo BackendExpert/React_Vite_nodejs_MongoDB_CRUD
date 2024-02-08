de7dce24a2881bdaeca0c20c50c957eb88e33334
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-08 08:49:13 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

---
+++ client/src/User/CreateUser.tsx
@@ -0,0 +1,9 @@
+
+
+const CreateUser = () => {
+  return (
+    <div>CreateUser</div>
+  )
+}
+
+export default CreateUser
---
+++ client/src/User/users.tsx
@@ -0,0 +1,6 @@
+
+export const users = () => {
+  return (
+    <div>users</div>
+  )
+}
