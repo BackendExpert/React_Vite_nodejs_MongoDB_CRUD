463d198576e571c396d983fd392665348126534d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-08 19:46:25 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/index.js
+++ server/index.js
@@ -2,14 +2,23 @@ const express = require('express')
 const mangoose = require('mongoose')
 const cors = require('cors')
 const { default: mongoose } = require('mongoose')
+//import userModel
+const UserModel = require('./models/UsersM')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
 //connect mongodb
-
 mongoose.connect("mongodb://127.0.0.1:27017/crud")
+
+// create APIs
+
+app.post("/createUser", (req, res) => {
+    UserModel.create(req.body)
+    .then(users => res.json(users))
+    .catch(err = res.json(err))
+})
 
 //testing server is runing
 app.listen(3001, () => {
