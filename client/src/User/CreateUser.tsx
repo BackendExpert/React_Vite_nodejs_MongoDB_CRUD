67d4cffe51f08117f00612e4b094cf40281065c5
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-08 12:58:38 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/User/CreateUser.tsx
+++ client/src/User/CreateUser.tsx
@@ -6,7 +6,7 @@ const CreateUser = () => {
   return (
     <div className="bg-blue-600 h-screen px-24 py-20">
         <div className="bg-white text-black px-12 py-8 rounded">
-            <Link to="/user"><span><IonIcon name="arrow-back" />Back</span></Link>
+            <Link to="/user"><span className='bg-blue-500 px-8 py-2 text-white rounded hover:bg-blue-600 duration-500'><IonIcon name="arrow-back" /><span className='pl-2 pt-[-2]'>Back</span></span></Link>
             <p className="font-semibold text-2xl py-4">Add New User</p>
             <hr />
 
