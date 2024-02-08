3f8a2bb368886dee92a8a85f1bd3d064217ff65d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-08 19:43:07 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/models/UsersM.js
+++ server/models/UsersM.js
@@ -1,3 +1,11 @@
+const { default: mongoose } = require('mongoose')
 const mangoose = require('mongoose')
 
-const UserSchema = new 
+const UserSchema = new mangoose.Schema({
+    name: String,
+    email: String,
+    age: Number
+})
+
+const UserModel = mongoose.model("user", UserSchema)
+module.exports = UserModel
