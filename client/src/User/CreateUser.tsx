498d4eb2f96322d055b226a347892a1449392bbf
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-08 09:49:35 +0530

    Udpate
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/User/CreateUser.tsx
+++ client/src/User/CreateUser.tsx
@@ -1,8 +1,16 @@
+import { Link } from 'react-router-dom'
+import IonIcon from "@reacticons/ionicons"
 
 
 const CreateUser = () => {
   return (
-    <div>CreateUser</div>
+    <div className="bg-blue-600 h-screen px-24 py-20">
+        <div className="bg-white text-black px-12 py-8 rounded">
+            <p className="font-semibold text-2xl py-4">Add New User</p>
+            <hr />
+
+        </div>
+    </div>
   )
 }
 
