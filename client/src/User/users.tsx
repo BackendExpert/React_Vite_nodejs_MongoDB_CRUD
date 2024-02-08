44867bc2a7e5025202609591ce3844bcdd9d14d0
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-08 09:39:48 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/User/users.tsx
+++ client/src/User/users.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react"
+import { Link } from 'react-router-dom'
+import IonIcon from "@reacticons/ionicons"
 
 const users = () => {
     const [allusers, setUser] = useState([{
@@ -9,9 +11,9 @@ return (
     <div className="bg-blue-600 h-screen px-24 py-20">
         <div className="bg-white text-black px-12 py-8 rounded">
             <p className="font-semibold text-2xl py-2">Users</p>
-            <hr />
-
-            <table className="table-auto w-full">
+            <hr className="pb-4"/>
+            <Link to="/user/create"><span className="bg-green-600 text-white px-8 py-2 mt-8">Add User <IonIcon name="person-add"/></span></Link>
+            <table className="table-auto w-full my-8">
                 <thead>
                     <tr>
                         <th>Name</th>
